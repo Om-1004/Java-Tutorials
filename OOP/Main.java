1ddeabdf9da6856855e8f300fb8b329c9d2011c7
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-15 17:24:11 -0500

    Update Main.java

--- OOP/Main.java
+++ OOP/Main.java
@@ -1,1 +1,29 @@
 
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Object = an instance of a class that may contain attributes and methods
+		
+//		Ball myBall = new Ball();
+		Ball myBall2 = new Ball();
+		/*	
+		System.out.println(myBall.size);
+		System.out.println(myBall.colour);
+		System.out.println(myBall.price);
+		System.out.println(myBall.cheap);
+
+		myBall.bounce();
+		myBall.kick();
+*/
+		System.out.println(myBall2.size);
+		System.out.println(myBall2.colour);
+		System.out.println(myBall2.price);
+		System.out.println(myBall2.cheap);
+		
+		myBall2.bounce();
+		myBall2.kick();
+	
+	}
+
+}
