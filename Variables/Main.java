bca0704f32312d0a9a9ac8a2645bd1aae8a8f11d
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-15 17:21:31 -0500

    Create Main.java

---
+++ Variables/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100;
+		long b = 100000000000000L;
+		float c = 3.14f;
+		double x = 12.6;
+		boolean y = true;
+		char character = '*';
+		String word = "Hello";
+		//System.out.println(x);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(character);
+		System.out.println(word);
+	}
+}
