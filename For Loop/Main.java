2efe7f51c46a4c1659ec7f99c768268607861303
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-15 16:25:16 -0500

    Create Main.java

---
+++ For Loop/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		for(int i=0; i<= 5; i++) {
+			System.out.println(i);
+		}
+		*/
+		
+		for (int i=10; i>=1; i--) {
+			System.out.println("T minus " + i + " seconds");
+		}
+		System.out.println("BLAST OFF!!!");
+	}
+
+}
