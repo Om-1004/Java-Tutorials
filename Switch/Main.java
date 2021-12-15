4c7cc58270ff866702017d5e40d6406c22b1a149
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-15 17:18:27 -0500

    Create Main.java

---
+++ Switch/Main.java
@@ -0,0 +1,64 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String month = "October";
+		
+		switch(month) {
+		case "Janurary":
+			System.out.println("It is Janurary");
+			break;
+			
+		case "February":
+			System.out.println("It is February");
+			break;
+			
+		case "March":
+			System.out.println("It is March");
+			break;
+			
+		case "April":
+			System.out.println("Its April");
+			break;
+			
+		case "May":
+			System.out.println("It is May");
+			break;
+			
+		case "June":
+			System.out.println("It is June");
+			break;
+			
+		case "July":
+			System.out.println("It is July");
+			break;
+			
+		case "August":
+			System.out.println("It is August");
+			break;
+			
+		case "September":
+			System.out.println("It is Janurary");
+			break;
+			
+		case "October":
+			System.out.println("It is October");
+			break;
+			
+		case "November":
+			System.out.println("It is November");
+			break;
+			
+		case "December":
+			System.out.println("It is December");
+			break;
+			
+		default:
+			System.out.println("This is not a month");
+			
+		}
+
+	}
+
+}
