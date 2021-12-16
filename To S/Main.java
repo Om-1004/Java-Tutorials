1cd0d5063628db2ecb765acec6c0f3a23159acb5
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-15 21:46:23 -0500

    Create Main.java

---
+++ To S/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		Book book = new Book();
+		/*
+		System.out.println(book.colour);
+		System.out.println(book.genre);
+		System.out.println(book.ratings);
+		System.out.println(book.price);
+		System.out.println(book.toString());
+
+		 */		
+		System.out.println(book);
+	}
+
+}
