b8b6a91c30ecc2a0810d059392cd60ba0fd15e43
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-16 21:03:11 -0500

    Create Main.java

---
+++ Inheritance/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		//car.ride();
+		
+		Bike bike = new Bike();
+		//bike.stop();
+		
+		System.out.println(car.windows);
+		System.out.println(bike.pedals);
+	}
+
+}
