c2d3c901b1e8db5dd322c8bc4537fd295145fc42
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-16 22:52:48 -0500

    Create Main.java

---
+++ Super Keyword/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		Hero hero_1 = new Hero("Superman",35, "Laser Eyes");
+		Hero hero_2 = new Hero("Batman" , 31, "Stealth");
+		//System.out.println(hero_1.name);
+		//System.out.println(hero_1.age);
+		//System.out.println(hero_1.power);
+		
+		 System.out.println(hero_2.toString());
+
+	}
+
+}
