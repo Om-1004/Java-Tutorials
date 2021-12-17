b18a2a3b141f172d7796570ec2cab5e28311879d
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-16 20:54:09 -0500

    Create Main.java

---
+++ Static Keyword/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		Dog dog1 = new Dog("Ghost");
+		Dog dog2 = new Dog("Hunter");
+		Dog dog3 = new Dog("Mason");
+		
+		//System.out.println(Dog.numberofDogs);
+		Dog.displayDogs();
+	}
+
+}
