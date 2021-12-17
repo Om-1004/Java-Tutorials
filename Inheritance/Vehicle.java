161f489aba6d0447ff6fcd0d0f9191425d28efd5
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2021-12-16 21:03:43 -0500

    Create Vehicle.java

---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+	
+	double speed;
+	double price;
+	
+	void ride() {
+		System.out.println("This vehicle is moving");
+	}
+	
+	void stop() {
+		System.out.println("This vehicle is stopped");
+	}
+
+}
